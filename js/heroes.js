2f64b1f73a015cdc0d96a50a383c8d9407f92268
andredezain <andre.sarizal.putra@gmail.com>
2017-01-20 18:59:32 +0700

    upload 1

---
+++ js/heroes.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+	$('#heroes-1').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/genji.png)');
+	    $('#nameheroes').html('genji');
+	    $('#descheroes').html('A cyborg ninja and deadly warrior who has found peace with his mechanical body.');
+	});
+	$('#heroes-2').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/mccree.png)');
+	    $('#nameheroes').html('mccree');
+	    $('#descheroes').html('An outlaw gunslinger who doles out justice on his own terms.');
+	});
+	$('#heroes-3').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/pharah.png)');
+	    $('#nameheroes').html('pharah');
+	    $('#descheroes').html('A decorated soldier who patrols the skies in her experimental Raptora combat suit.');
+	});
+	$('#heroes-4').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/reaper.png)');
+	    $('#nameheroes').html('reaper');
+	    $('#descheroes').html('A mercenary and ruthless killer who hunts the former agents of Overwatch.');
+	});
+	$('#heroes-5').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/soldier76.png)');
+	    $('#nameheroes').html('soldier-76');
+	    $('#descheroes').html('A vigilante who will stop at nothing to bring the enemies of Overwatch to justice.');
+	});
+	$('#heroes-6').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/sombra.png)');
+	    $('#nameheroes').html('sombra');
+	    $('#descheroes').html('A notorious hacker, on the hunt for secrets—and the power they bring.');
+	});
+	$('#heroes-7').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/tracer.png)');
+	    $('#nameheroes').html('tracer');
+	    $('#descheroes').html('A former Overwatch agent, time-jumping adventurer, and irrepressible force for good.');
+	});
+	$('#heroes-8').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/bastion.png)');
+	    $('#nameheroes').html('bastion');
+	    $('#descheroes').html('A transforming robot who explores the world; fascinated by nature, but wary of humanity.');
+	});
+	$('#heroes-9').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/hanzo.png)');
+	    $('#nameheroes').html('hanzo');
+	    $('#descheroes').html('A lethal bowman and assassin without peer.');
+	});
+	$('#heroes-10').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/junkrat.png)');
+	    $('#nameheroes').html('junkrat');
+	    $('#descheroes').html('An explosives-obsessed freak who lives to cause chaos and destruction.');
+	});
+	$('#heroes-11').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/mei.png)');
+	    $('#nameheroes').html('mei');
+	    $('#descheroes').html('A specialist in weather manipulation who has taken up the fight to preserve the environment.');
+	});
+	$('#heroes-12').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/torbjorn.png)');
+	    $('#nameheroes').html('torbjorn');
+	    $('#descheroes').html('An ingenious engineer who forges weapons systems on the battlefield.');
+	});
+	$('#heroes-13').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/widowmaker.png)');
+	    $('#nameheroes').html('widowmaker');
+	    $('#descheroes').html('A perfect assassin: patient, ruthlessly efficient, and without emotion or remorse.');
+	});
+	$('#heroes-14').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/dva.png)');
+	    $('#nameheroes').html('d.va');
+	    $('#descheroes').html('A former pro gamer who now pilots a state-of-the-art mech in defense of her homeland.');
+	});
+	$('#heroes-15').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/reinhardt.png)');
+	    $('#nameheroes').html('reinhardt');
+	    $('#descheroes').html('A champion of a bygone age, who lives by the knightly codes of valor, justice, and courage.');
+	});
+	$('#heroes-16').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/roadhog.png)');
+	    $('#nameheroes').html('roadhog');
+	    $('#descheroes').html('A ruthless killer with tremendous strength and a reputation for cruelty and wanton destruction.');
+	});
+	$('#heroes-17').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/winston.png)');
+	    $('#nameheroes').html('winston');
+	    $('#descheroes').html('A super-intelligent, genetically engineered gorilla, scientist, and champion for humanity\'s potential.');
+	});
+	$('#heroes-18').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/zarya.png)');
+	    $('#nameheroes').html('zarya');
+	    $('#descheroes').html('One of the world\'s strongest women, who sacrificed personal glory to protect her country.');
+	});
+	$('#heroes-19').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/ana.png)');
+	    $('#nameheroes').html('ana');
+	    $('#descheroes').html('This Overwatch stalwart has emerged from years of seclusion to defend a new generation.');
+	});
+	$('#heroes-20').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/lucio.png)');
+	    $('#nameheroes').html('lucio');
+	    $('#descheroes').html('An international celebrity who fights for social change through his music and actions.');
+	});
+	$('#heroes-21').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/mercy.png)');
+	    $('#nameheroes').html('mercy');
+	    $('#descheroes').html('A guardian angel, peerless healer, brilliant medical researcher, and staunch advocate for peace.');
+	});
+	$('#heroes-22').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/symmetra.png)');
+	    $('#nameheroes').html('symmetra');
+	    $('#descheroes').html('An architech who manipulates hard-light constructs to build her perfect, ordered world.');
+	});
+	$('#heroes-23').click(function(){
+	    $('#viewheroes').css('background-image', 'url(img/heroes/zenyatta.png)');
+	    $('#nameheroes').html('zenyatta');
+	    $('#descheroes').html('An omnic monk who wanders the world in search of spiritual enlightenment.');
+	});
+
+});
